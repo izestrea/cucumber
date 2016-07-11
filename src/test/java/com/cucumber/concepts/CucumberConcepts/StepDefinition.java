d2f7727a14d1647db0db6fbe02a803d18262e51b
izestrea <izestrea@gmail.com>
2016-07-11 17:18:46 +0300

    Remove old code

--- src/test/java/com/cucumber/concepts/CucumberConcepts/StepDefinition.java
+++ src/test/java/com/cucumber/concepts/CucumberConcepts/StepDefinition.java
@@ -21,56 +21,47 @@ public class StepDefinition extends AbstractPageStepDef {
 
     @Given("^I am on the web site$")
     public void I_am_on_the_web_site() throws Throwable {
-//        driver.navigate().to("http://www.thetestroom.com/webapp/");
         landingPage = new LandingPage(driver);
         landingPage.navigateToWebApp();
     }
 
     @When("^I click on the \"([^\"]*)\"$")
     public void I_click_on_the(String link) throws Throwable {
-//        driver.findElement(By.id(link.toLowerCase() + "_link")).click();
         contactPage = landingPage.navigateToContactPage(link);
     }
 
     @And("^I enter \"([^\"]*)\" into the name field$")
     public void I_enter_into_the_name_field(String value) throws Throwable {
-//        driver.findElement(By.name("name_field")).sendKeys(value);
         contactPage.setNameField(value);
     }
 
     @And("^I enter \"([^\"]*)\" into the address field$")
     public void I_enter_into_the_address_field(String value) throws Throwable {
-//        driver.findElement(By.name("address_field")).sendKeys(value);
         contactPage.setAddressField(value);
     }
 
     @And("^I enter \"([^\"]*)\" into the poscode field$")
     public void I_enter_into_the_poscode_field(String value) throws Throwable {
-//        driver.findElement(By.name("postcode_field")).sendKeys(value);
         contactPage.setPostcodeField(value);
     }
 
     @And("^I enter \"([^\"]*)\" into the email field$")
     public void I_enter_into_the_email_field(String value) throws Throwable {
-//        driver.findElement(By.name("email_field")).sendKeys(value);
         contactPage.setEmailField(value);
     }
 
     @And("^I submit the contact form$")
     public void I_submit_the_contact_form() throws Throwable {
-//        driver.findElement(By.id("submit_message")).click();
         contactConfirmPage = contactPage.submitForm();
     }
 
     @Then("^I check I am on the confirmation page$")
     public void I_check_I_am_on_the_confirmation_page() throws Throwable {
-//        Assert.assertTrue(driver.findElement(By.cssSelector(".content h1")).getText().contains("We have your message"));
         Assert.assertTrue(contactConfirmPage.getPageTitle().contains("We have your message"));
     }
 
     @Then("^I close browser$")
     public void I_close_browser() throws Throwable {
-//        driver.quit();
         contactConfirmPage.closeDriver();
     }
 }
